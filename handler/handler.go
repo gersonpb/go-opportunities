841ab899ff8932bc2d3fbcda0fe3c2ff3e702617
gersonpb <gersonpb@gmail.com>
2024-08-18 19:17:27 -0300

    feat: separete files routes

--- handler/handler.go
+++ handler/handler.go
@@ -1,33 +1,3 @@
 package handler
 
-import (
-	"net/http"
 
-	"github.com/gin-gonic/gin"
-)
-
-func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "ShowOpeningHandler",
-	})
-}
-func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "CreateOpeningHandler",
-	})
-}
-func DeleteOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "DeleteOpeningHandler",
-	})
-}
-func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "UpdateOpeningHandler",
-	})
-}
-func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "ListOpeningsHandler",
-	})
-}
