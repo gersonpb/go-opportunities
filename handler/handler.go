ea9041863b95675663c766d9c0e3a05fc4082a6e
gersonpb <gersonpb@gmail.com>
2024-08-21 21:53:09 -0300

    feat: add creating opning logics

--- handler/handler.go
+++ handler/handler.go
@@ -1,3 +1,17 @@
 package handler
 
+import (
+	"github.com/gersonpb/go-opportunities/config"
+	"gorm.io/gorm"
+)
 
+
+var (
+	logger *config.Logger
+	db *gorm.DB
+)
+
+func InitializedHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
