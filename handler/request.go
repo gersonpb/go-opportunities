5b1f7fccd056ffebb889f616d19edc95742494b9
gersonpb <gersonpb@gmail.com>
2024-08-22 22:12:48 -0300

    update listOpening

--- handler/request.go
+++ handler/request.go
@@ -5,6 +5,7 @@ import "fmt"
 func errParamIsRequired(name, typ string) error {
 	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
 }
+
 type CreateOpeningRequest struct {
 	Role 	 string	`json:"role"`
 	Company  string	`json:"company"`
